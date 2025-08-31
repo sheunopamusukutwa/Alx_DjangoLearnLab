effe299138a599285a4cec3ab824e8aa1eb682b1
SheunopaMusukutwa <scmusukutwa@gmail.com>
2025-08-31 12:42:13 +0200

    Admin interface

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -5,5 +5,15 @@ from .models import Book
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
+    # Columns shown in the list view
     list_display = ("title", "author", "publication_year")
+
+    # Right-side filters
+    list_filter = ("publication_year", "author")
+
+    # Top search box (case-insensitive icontains)
     search_fields = ("title", "author")
+
+    # Optional niceties
+    ordering = ("title",)                 # default sort
+    list_per_page = 25 
