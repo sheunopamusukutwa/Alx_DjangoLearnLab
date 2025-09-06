6c7598fd4c26d3ff6dce7c1ca25bb5e7e972bcf2
SheunopaMusukutwa <scmusukutwa@gmail.com>
2025-09-06 21:51:00 +0200

    Snatche

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login   
-from django.contrib.auth.decorators import login_required, user_passes_test, permission_required
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.decorators import permission_required
 from django import forms
 from .models import Book
 from .models import Library  
