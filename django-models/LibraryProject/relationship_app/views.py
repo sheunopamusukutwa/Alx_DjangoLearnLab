56fb405f1020803a0b0fe51fda2ea6f2a97c0730
SheunopaMusukutwa <scmusukutwa@gmail.com>
2025-09-06 20:39:42 +0200

    tricky project

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,6 @@
-from django.views.generic import DetailView
-from .models import Library   
+from django.shortcuts import render
+from .models import Book
 
-class LibraryDetailView(DetailView):
-    model = Library
-    template_name = "relationship_app/library_detail.html"
-    context_object_name = "library"
+def list_books(request):
+    books = Book.objects.all()  # <- keeps the other required pattern
+    return render(request, "relationship_app/list_books.html", {"books": books})
