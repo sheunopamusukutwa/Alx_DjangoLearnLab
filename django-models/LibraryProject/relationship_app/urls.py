c6194a8eac0519e2972ec061688c3f5a7540273c
SheunopaMusukutwa <scmusukutwa@gmail.com>
2025-09-06 21:55:31 +0200

    done

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -21,8 +21,7 @@ urlpatterns = [
     path("roles/librarian/", views.librarian_view, name="librarian_view"),
     path("roles/member/", views.member_view, name="member_view"),
 
-    path("books/add/", views.add_book, name="add_book"),
-    path("books/<int:pk>/edit/", views.edit_book, name="edit_book"),
-    path("books/<int:pk>/delete/", views.delete_book, name="delete_book"),
-]
+path("add_book/", views.add_book, name="add_book"),                    # <-- contains "add_book/"
+    path("edit_book/<int:pk>/", views.edit_book, name="edit_book"),        # <-- contains "edit_book/"
+    path("delete_book/<int:pk>/", views.delete_book, name="delete_book"),
 ]
