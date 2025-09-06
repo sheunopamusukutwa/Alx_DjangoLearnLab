e79dcca70ab6b848599b542dabeba6d20ad26b2e
SheunopaMusukutwa <scmusukutwa@gmail.com>
2025-09-06 21:20:56 +0200

    profiles

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -6,11 +6,15 @@ from .views import list_books, LibraryDetailView
 app_name = "relationship_app"
 
 urlpatterns = [
+
     path("books/", list_books, name="list_books"),
     path("libraries/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
 
-    # Auth routes
     path("login/",  LoginView.as_view(template_name="relationship_app/login.html"),  name="login"),
     path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
-    path("register/", views.register, name="register"),   # <-- EXACT: "views.register"
+    path("register/", views.register, name="register"),
+
+    path("roles/admin/", views.admin_view, name="admin_view"),
+    path("roles/librarian/", views.librarian_view, name="librarian_view"),
+    path("roles/member/", views.member_view, name="member_view"),
 ]
