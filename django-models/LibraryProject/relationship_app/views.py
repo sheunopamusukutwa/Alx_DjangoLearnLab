17c1d1316ee0a8d0103a10cc9be77d5e7df59e96
SheunopaMusukutwa <scmusukutwa@gmail.com>
2025-09-06 20:47:54 +0200

    update views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 
 from .models import Book
-from .models import Library 
+from .models import Library
 
 def list_books(request):
     books = Book.objects.all()
