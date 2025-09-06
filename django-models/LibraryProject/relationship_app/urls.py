abf9ab797487f96fd4379634985dd4529f6d3519
SheunopaMusukutwa <scmusukutwa@gmail.com>
2025-09-06 21:06:49 +0200

    update URLs

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import list_books, LibraryDetailView, register
+from . import views                                      # <-- add this
+from .views import list_books, LibraryDetailView          # <-- keep these for other checks
 
 app_name = "relationship_app"
 
 urlpatterns = [
-    # existing views
     path("books/", list_books, name="list_books"),
     path("libraries/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
 
-    # auth
+    # auth routes
     path("login/",  LoginView.as_view(template_name="relationship_app/login.html"),  name="login"),
     path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
-    path("register/", register, name="register"),
+    path("register/", views.register, name="register"),   # <-- satisfies "views.register"
 ]
