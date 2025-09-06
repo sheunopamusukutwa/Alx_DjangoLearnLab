20a13f89a8d8193c83ead21b50ed7a9666473203
SheunopaMusukutwa <scmusukutwa@gmail.com>
2025-09-06 21:14:56 +0200

    task 3

--- django-models/LibraryProject/relationship_app/apps.py
+++ django-models/LibraryProject/relationship_app/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 class RelationshipAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'LibraryProject.relationship_app'
+
+    def ready(self):
+        from . import signals  # noqa: F401
