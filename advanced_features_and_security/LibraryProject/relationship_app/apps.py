588e3399928d59786d01471b3640e8a639c191e3
sheunopamusukutwa <scmusukutwa@gmail.com>
2025-09-27 20:25:36 +0200

    Committing for the first time on my new machine!

--- advanced_features_and_security/LibraryProject/relationship_app/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-class RelationshipAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'LibraryProject.relationship_app'
-
-    def ready(self):
-        from . import signals  # noqa: F401
