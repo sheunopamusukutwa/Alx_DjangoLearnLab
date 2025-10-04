5a3f33346067b42d16606d5b4cb11ac1469467b1
sheunopamusukutwa <scmusukutwa@gmail.com>
2025-10-04 12:03:01 +0200

    add User import

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,5 +1,7 @@
-from django.conf import settings
 from django.db import models
+from django.contrib.auth.models import User  # required by ALX checker
+from django.conf import settings
+
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
